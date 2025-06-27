b314922e4af17559895ce4ca33b3d14bd60ea3a4
GitLab CI <dubvla.1401@gmail.com>
2025-06-27 16:17:20 +0300

    Update enums

--- struct/enum_2/enum_2.sv
+++ struct/enum_2/enum_2.sv
@@ -1,13 +1,17 @@
 module main;
 
-    typedef enum logic [1:0] {
-        IDLE,
-        RUNNING,
-        STOPPED
-    } state_t;
+    typedef enum logic [7:0] {
+        IDLE       = 0,
+        INITIALIZING = 1,
+        RUNNING    = 5,
+        PAUSED     = 8,
 
-    state_t current_state;
+        STOPPED    = RUNNING + 3,
+        ERROR_OCCURRED = ERROR_CODE_BASE + 1,
+        WARNING_ACTIVE = (STATUS_OFFSET * 2) + 1,
+        FINAL_STATE = STOPPED + INITIALIZING + IDLE
+    } system_state_t;
 
-    
+    system_state_t current_state;
 
 endmodule
